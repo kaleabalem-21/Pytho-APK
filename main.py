0eb2c4afc9c10fb22e4ac5db8c89c5d85a8ecc2f
kaleabalem-21 <kalabalem793@gmail.com>
2026-08-11 03:09:53 +0300

    Update label text and font in MyApp

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class MyApp(App):
-    def build(self):
-        return Label(text='Hello ጀግናው! My First App', font_size=50)
 
-if __name__ == '__main__':
-    MyApp().run()
+class MyApp(App):
+
+  def build(self):
+="Hello ጀግናው!", font_name="nyala.ttf")
+
+
+if __name__ == "__main__":
+  MyApp().run()
